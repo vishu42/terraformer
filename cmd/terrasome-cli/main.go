7ae38cdb31988bbab027f5c10f4be687147d1c46
vishu42 <tewatiavishal3@gmail.com>
2023-06-17 20:46:25 +0530

    update the directory structure

--- cmd/terrasome-cli/main.go
+++ cmd/terrasome-cli/main.go
@@ -3,7 +3,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/vishu42/terrasome/cmd/client/cmd"
+import "github.com/vishu42/terrasome/cmd/terrasome-cli/cmd"
 
 func main() {
 	cmd.Execute()
