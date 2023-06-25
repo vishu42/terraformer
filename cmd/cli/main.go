cfaf3ece656bc99e9d639b00d1e19344d0045ecf
vishu42 <tewatiavishal3@gmail.com>
2023-06-25 14:46:02 +0530

    fix import path after rename

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,7 +3,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/vishu42/terrasome/cmd/terrasome-cli/cmd"
+import "github.com/vishu42/terrasome/cmd/cli/cmd"
 
 func main() {
 	cmd.Execute()
