eb235f4977d30c6e8406136b264aace9fb7aa6c4
vishu42 <tewatiavishal3@gmail.com>
2023-07-21 12:10:54 +0530

    add some functionality to login command

---
+++ cmd/cli/cmd/run/login.go
@@ -0,0 +1,59 @@
+package run
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/microsoft"
+)
+
+func RunLogin(cmd *cobra.Command, args []string) {
+	// Replace with your Azure App Registration configuration
+	clientID := "292def6c-3c4c-42ed-9ef2-8a197c7abc33"
+	// clientSecret := "dlG8Q~n5mpzNhWQPGHMFFzTCtb-i6q0FNm066a50"
+	redirectURL := "http://localhost:8000/callback"
+	scopes := []string{"api://292def6c-3c4c-42ed-9ef2-8a197c7abc33/SCOPE1"} // Specify the required scopes
+
+	conf := &oauth2.Config{
+		ClientID:    clientID,
+		RedirectURL: redirectURL,
+		Scopes:      scopes,
+		Endpoint:    microsoft.AzureADEndpoint("4d16a70b-76a1-4ad7-944a-13513528982b"), // Replace with your Azure AD tenant ID
+	}
+
+	// Generate authorization URL
+	authURL := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+
+	fmt.Printf("Open the following URL in your browser:\n\n%s\n\n", authURL)
+	fmt.Println("After authentication, you will be redirected to the specified redirect URL.")
+
+	m := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: m,
+	}
+	// Start a local HTTP server to listen for the callback
+	m.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+
+		// Exchange authorization code for access token
+		token, err := conf.Exchange(context.Background(), code)
+		if err != nil {
+			log.Fatal("Token exchange error:", err)
+		}
+
+		fmt.Println("Token:", token.AccessToken)
+
+		// Shut down the local server
+		fmt.Println("\nAuthentication successful! You can close the browser.")
+		w.Write([]byte("Authentication successful! You can close the browser."))
+		// server.Shutdown(r.Context())
+	})
+
+	log.Fatal(server.ListenAndServe())
+}
--- go.mod
+++ go.mod
@@ -5,9 +5,14 @@ go 1.20
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/spf13/cobra v1.7.0
+	golang.org/x/oauth2 v0.10.0
 )
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
